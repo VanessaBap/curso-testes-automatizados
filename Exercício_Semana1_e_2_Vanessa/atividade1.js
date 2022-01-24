ecb5e8b3ee03798a3955beecf142b879f4c6786c
VanessaBap <97605829+VanessaBap@users.noreply.github.com>
2022-01-24 18:11:47 -0300

    Add files via upload
    Manipulação de strings
    
    Exercícios de classe de teste que realiza manipulação de strings
    
    Signed-off-by: Vanessa Baptista <vanessa.baptista@venturus.org.br>

---
+++ Exercício_Semana1_e_2_Vanessa/atividade1.js
@@ -0,0 +1,72 @@
+class StringManipulations {
+
+    /**
+     * Class for string manipulations
+     * @param  {String} string  
+     */
+    constructor(string) {
+        this.string = string;
+    }
+
+    /**
+     * Returns the first substring that matches the given string
+     * @param  {String} subStr  substring to be matched
+     * @return {String}
+     */
+    findFirstMatch(subStr) {
+        return subStr.substring(0,1);
+    }
+
+    /**
+     * Returns the last substring that matches the given string
+     * @param  {String} subStr  substring to be matched
+     * @return {String}
+     */
+    findLastMatch(subStr) {
+        return subStr.substring(subStr.length - 1);
+    }
+
+    /**
+     * Returns the fsubstring between two given other strings
+     * @param  {String} subStr1  begining of the match
+     * @param  {String} subStr2  ending of the match
+     * @return {String}
+     */
+    substringBetweenMatches(subStr1, subStr2) {
+        let begin = this.string.indexOf(subStr1) + subStr1.length;
+        let end = this.string.indexOf(subStr2);
+        return this.string.substring(begin, end);
+    }
+
+    /**
+    Given the string attribute of the class, 
+    return a string made of the first 2
+    and the last 2 chars of the original string.
+    If the string length is less than 2, 
+    return instead the empty string.
+    * @return {String}
+    */
+    both_ends() {
+        if (this.string.length < 2)
+          return "";
+        return this.string.substring(0, 2) + this.string.substring(this.string.length - 2, this.string.length);
+
+    }
+
+    /**
+     Given a string, return a string
+    where all occurences of its first char have
+    been changed to '*', except do not change
+    the first char itself.
+    e.g. 'babble' yields 'ba**le'
+    * @param  {String} str1  
+    * @return {String}
+    */
+    fix_start(str1) {
+        let firstChar = str1.charAt(0);
+        return str1.replace(firstChar, "*");
+    }
+}
+
+
+module.exports = StringManipulations;
